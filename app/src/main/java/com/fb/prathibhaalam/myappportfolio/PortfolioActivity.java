2c569f2250be5a4bb2a75160fcd2fa2346fafa79
Prathibha Bharathi Alam <prathibha.alam@gmail.com>
2015-07-21 18:38:03 -0700

    addressing review comments

--- app/src/main/java/com/fb/prathibhaalam/myappportfolio/PortfolioActivity.java
+++ app/src/main/java/com/fb/prathibhaalam/myappportfolio/PortfolioActivity.java
@@ -1,7 +1,7 @@
 package com.fb.prathibhaalam.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,59 +14,22 @@ public class PortfolioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
+    }
 
-        final Button spotifyStreamerAppButton = (Button) findViewById(R.id.spotify_streamer_app_button);
-        spotifyStreamerAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PortfolioActivity.this, "This button will launch my Spotify Streamer app", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void buttonClick(View view) {
+        displayText(String.format(getString(R.string
+                .app_toast_text), ((Button) view).getText()));
+    }
 
-        final Button footballScoreAppButton = (Button) findViewById(R.id.football_scores_app_button);
-        footballScoreAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PortfolioActivity.this, "This button will launch my Football Scores app", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        final Button libraryAppButton = (Button) findViewById(R.id.library_app_button);
-        libraryAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PortfolioActivity.this, "This button will launch my Library app", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        final Button buildItBiggerAppButton = (Button) findViewById(R.id.build_it_bigger_app_button);
-        buildItBiggerAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PortfolioActivity.this, "This button will launch my Build it bigger app", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        final Button xyzReaderAppButton = (Button) findViewById(R.id.xyz_reader_app_button);
-        xyzReaderAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PortfolioActivity.this, "This button will launch my Xyz Reader app", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        final Button capstoneAppButton = (Button) findViewById(R.id.capstone_app_button);
-        capstoneAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PortfolioActivity.this, "This button will launch my Capstone app", Toast.LENGTH_SHORT).show();
-            }
-        });
+    private void displayText(CharSequence s) {
+        Toast.makeText(getApplicationContext(), s, Toast
+                .LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu;
+        // this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_portfolio, menu);
         return true;
     }
@@ -77,7 +40,6 @@ public class PortfolioActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
